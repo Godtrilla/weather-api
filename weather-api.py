c63b9157efa5a05ae06fa9b97d7d69b8e7113021
Dee Jackson <dtjackso@uark.edu>
2022-03-21 01:42:22 -0400

    refactored global variables

--- weather-api.py
+++ weather-api.py
@@ -4,28 +4,29 @@ import requests
 import sys
 import json
 
-key = sys.argv[1]
-filepath = sys.argv[2]
-url = 'http://api.openweathermap.org/data/2.5/weather?'
-cities = ["London", "Boston"]
-weather = []
+
+KEY = sys.argv[1]
+FILEPATH = sys.argv[2]
+URL = 'http://api.openWEATHERmap.org/data/2.5/WEATHER?'
+CITIES = ["London", "Boston"]
+WEATHER = []
 
 def call_api():
-    for i in range(len(cities)):
-        response = requests.get(url, params={'units': 'imperial', 'APPID': key, 'q': cities[i]})
+    for i in range(len(CITIES)):
+        response = requests.get(URL, params={'units': 'imperial', 'APPID': KEY, 'q': CITIES[i]})
         date = response.headers["date"]
         response = response.json()
-        payload = [response["name"], date, response["main"]["temp"], response["weather"][0]["description"],
+        payload = [response["name"], date, response["main"]["temp"], response["WEATHER"][0]["description"],
                    response["main"]["pressure"], response["main"]["humidity"]]
-        weather.append(payload)
-    weather.append(helper())
-    csv_writer(weather)
+        WEATHER.append(payload)
+    WEATHER.append(helper())
+    csv_writer(WEATHER)
 
 def helper():
-    response = requests.get(url, params={'units': 'imperial', 'APPID': key, 'lon': -122.41, 'lat': 37.77})
+    response = requests.get(URL, params={'units': 'imperial', 'APPID': KEY, 'lon': -122.41, 'lat': 37.77})
     date = response.headers["date"]
     response = response.json()
-    payload = [response["name"], date, response["main"]["temp"], response["weather"][0]["description"],
+    payload = [response["name"], date, response["main"]["temp"], response["WEATHER"][0]["description"],
                response["main"]["pressure"], response["main"]["humidity"]]
     return payload
 
